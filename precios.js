25913f26be0556d3f2cca9b28b888f6261df754d
MagoCitu357 <137245741+MagoCitu357@users.noreply.github.com>
2025-05-18 16:17:35 -0300

    v1

---
+++ precios.js
@@ -0,0 +1,17 @@
+const precios = {
+  // Merienda:
+  TatinN: 500,
+  TatinB: 500,
+  Pebete: 1000,
+  Papitas: 600,
+  // Caramelos:
+  Chupetin: 150,
+  ChicleM: 900,
+  ChicleF: 900,
+  // Almuerzo:
+  Sanguche_Milanesa: "???",
+  // Bebidas.
+  Coca_cola: 1000,
+  Sprite: 1000,
+  Bagio: "???",
+};
---
+++ script.js
@@ -0,0 +1,15 @@
+// Merienda:
+document.getElementById("precio-tatinN").textContent = `$${precios.TatinN}`;
+document.getElementById("precio-tatinB").textContent = `$${precios.TatinB}`;
+document.getElementById("precio-pebete").textContent = `$${precios.Pebete}`;
+document.getElementById("precio-papitas").textContent = `$${precios.Papitas}`;
+// Caramelos:
+document.getElementById("precio-chupetin").textContent = `$${precios.Chupetin}`;
+document.getElementById("precio-chicleM").textContent = `$${precios.ChicleM}`;
+document.getElementById("precio-chicleF").textContent = `$${precios.ChicleF}`;
+// Almuerzo:
+document.getElementById("precio-milanesa").textContent = `$${precios.Sanguche_Milanesa}`;
+// Bebidas:
+document.getElementById("precio-cocacola").textContent = `$${precios.Coca_cola}`;
+document.getElementById("precio-sprite").textContent = `$${precios.Sprite}`;
+document.getElementById("precio-bagio").textContent = `$${precios.Bagio}`;
